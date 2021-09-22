ff1316983be320c21375d3a860109d6e613d28d5
syuneara <89122051+syuneara@users.noreply.github.com>
2021-09-22 21:53:48 +0900

    add

---
+++ hooks/useEscapeKey.js
@@ -0,0 +1,19 @@
+import { useCallback, useEffect } from "react";
+
+export default function useEscapeKey(handler) {
+  const escFunction = useCallback((event) => {
+    if (event.keyCode === 27) {
+      handler(event);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keydown", escFunction, false);
+
+    return () => {
+      document.removeEventListener("keydown", escFunction, false);
+    };
+  }, [escFunction]);
+
+  return null;
+}
---
+++ pages/api/website/index.js
@@ -0,0 +1,48 @@
+import { createWebsite, getWebsiteById, updateWebsite } from "lib/queries";
+import { useAuth } from "lib/middleware";
+import { getRandomChars, uuid } from "lib/crypto";
+import { methodNotAllowed, ok, unauthorized } from "lib/response";
+
+export default async (req, res) => {
+  await useAuth(req, res);
+
+  const { user_id, is_admin } = req.auth;
+  const { website_id, enable_share_url } = req.body;
+
+  if (req.method === "POST") {
+    const { name, domain } = req.body;
+
+    if (website_id) {
+      const website = await getWebsiteById(website_id);
+
+      if (website.user_id !== user_id && !is_admin) {
+        return unauthorized(res);
+      }
+
+      let { share_id } = website;
+
+      if (enable_share_url) {
+        share_id = share_id ? share_id : getRandomChars(8);
+      } else {
+        share_id = null;
+      }
+
+      await updateWebsite(website_id, { name, domain, share_id });
+
+      return ok(res);
+    } else {
+      const website_uuid = uuid();
+      const share_id = enable_share_url ? getRandomChars(8) : null;
+      const website = await createWebsite(user_id, {
+        website_uuid,
+        name,
+        domain,
+        share_id,
+      });
+
+      return ok(res, website);
+    }
+  }
+
+  return methodNotAllowed(res);
+};
